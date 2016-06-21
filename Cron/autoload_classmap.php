b2f3199e1e702f90d846bbb34aea34cff14647dc
DeepakKumar <deepak.kumar@osscube.com>
2016-06-21 13:10:39 +0530

    Cron Module Of ZF2 Commiting

--- Cron/autoload_classmap.php
+++
@@ -1,7 +0,0 @@
-<?php
- return array(
-    /*'PPLib\PlanningproController' => __DIR__ . '/../../vendor/PPLib/PlanningproController.php',
-    'PPLib\ErrorHandling' => __DIR__ . '/../../vendor/PPLib/ErrorHandling.php',
-    'PPLib\PlanningproClientController' => __DIR__ . '/../../vendor/PPLib/PlanningproClientController.php',
-    'PPLib\PageAccess' => __DIR__ . '/../../vendor/PPLib/PageAccess.php',*/
-); 
